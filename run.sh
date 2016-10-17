abecff60e5bfc44ee26444b94d5fe1411a7cdccf
Afaren <550047450@qq.com>
2016-10-17 19:23:50 +0800

    afaren - add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: run.sh
+# Author: Afar
+# mail: 550047450@qq.com
+# Created Time: Monday, October 17, 2016 PM07:10:23 CST
+#########################################################################
+#!/bin/bash
+
+firefox localhost:8080/jersey-demo/hello 
